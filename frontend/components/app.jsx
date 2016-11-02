b3db7ff3dd94c7deb07345f25dd5daa03cc2b097
can lekili <clekili@gmail.com>
2016-11-01 22:43:45 -0700

    add styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Splash from './splash/splash';
 import NavBar from './navbar/navbar';
+import Footer from './footer/footer';
 
 const App = ({ children }) => (
     <div>
       <NavBar />
+      <Splash />
       {children}
+      <Footer />
     </div>
 );
 
