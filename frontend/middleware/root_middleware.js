f982f11ad73403350bf1288e5c568a412ad4ecac
can lekili <clekili@gmail.com>
2016-11-02 17:05:18 -0700

    add video reducer and middleware to root middleware and reducer

--- frontend/middleware/root_middleware.js
+++ frontend/middleware/root_middleware.js
@@ -1,8 +1,10 @@
 import { applyMiddleware } from 'redux';
 import SessionMiddleware from './session_middleware';
+import VideosMiddleware from './videos_middleware';
 
 const RootMiddleware = applyMiddleware(
-  SessionMiddleware
+  SessionMiddleware,
+  VideosMiddleware
 );
 
 export default RootMiddleware;
