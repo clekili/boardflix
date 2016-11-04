ebeb28aae0c57033ff50b73517918725013622ed
can lekili <clekili@gmail.com>
2016-11-03 23:25:32 -0700

    edit video carousel component

--- frontend/components/videos/video_carousel.jsx
+++ frontend/components/videos/video_carousel.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 const VideoCarousel = (props) => {
+  var settings = {
+        slidesToShow: 3,
+  		  slidesToScroll: 2
+      };
+
   return (
-  <div>
-    <h1>{props.category}</h1>
-    { props.videos.map((v, i) => ( <p key={i}> {v.name} </p> )) }
-  </div>);
+    <div>
+      <h1>{props.category}</h1>
+      <div className="carouselContainer">
+        <Slider {...settings}>
+          { props.videos.map((v, i) => (
+            <div>
+              <h2 key={i} className="videoTitle"> {v.name} </h2>
+              <img className="videoThumbnail" src={getVideoThumbnail(v)}/>
+            </div>
+          )) }
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+
+const getVideoThumbnail = (video) => {
+  return `https://img.youtube.com/vi/${video.youtube_id}/hqdefault.jpg`;
 };
 
 export default VideoCarousel;
