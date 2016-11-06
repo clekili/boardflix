e68b9e7b5d149c98d363fa25a5f8d72556f811b6
can lekili <clekili@gmail.com>
2016-11-06 11:58:48 -0800

    fix footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () =>(
   <footer className='footer'>
-    <p>© 2016 Speechify. All rights reserved.</p>
+    <p>© 2016 Boardflix. All rights reserved.</p>
     <ul className='footerList'>
       <li key='1'>
         <a href="#">About</a>
