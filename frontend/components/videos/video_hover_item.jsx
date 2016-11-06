99cc9db5912bd41c721d2f473e06171c96ab7c05
can lekili <clekili@gmail.com>
2016-11-06 15:33:18 -0800

    style video hover item

--- frontend/components/videos/video_hover_item.jsx
+++ frontend/components/videos/video_hover_item.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 import VideoDialogContainer from './video_dialog_container';
 import DeleteButtonContainer from './delete_button_container';
+import FlatButton from 'material-ui/FlatButton';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class VideoHoverItem extends React.Component {
   constructor(props){
@@ -17,13 +21,21 @@ class VideoHoverItem extends React.Component {
         <Link
           className="playLink"
           to={`${this.props.video.category}/videos/${this.props.video.id}`}>
-          Play
+          <img
+            src="http://res.cloudinary.com/ddqzltwv6/image/upload/v1478471722/play_da9o38.png" className="playLinkImg" alt="play" />
         </Link>
         <br/>
         <VideoDialogContainer video={this.props.video}/>
         <DeleteButtonContainer video={this.props.video}/>
         <br/>
-        <button className="videoDetailsBtn">Details</button>
+          <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <FlatButton
+              backgroundColor=""
+              hoverColor=""
+              label="Details"
+              className="videoDetailsBtn"
+            />
+          </MuiThemeProvider>
       </div>
     );
   }
