6931c77e5b9b9590157414f8f22c172e0dd5e981
can lekili <clekili@gmail.com>
2016-11-06 16:56:00 -0800

    implement comments reducer

--- frontend/reducers/comments_reducer.js
+++ frontend/reducers/comments_reducer.js
@@ -1,7 +1,29 @@
-
+import { RECEIVE_COMMENTS,
+         RECEIVE_COMMENT,
+         RECEIVE_ERRORS,
+         REMOVE_COMMENT } from '../actions/comment_actions';
+import merge from 'lodash/merge';
 
 const CommentsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let newState = merge({}, oldState);
 
+  switch (action.type) {
+    case RECEIVE_COMMENTS:
+      newState = action.comments;
+      break;
+    case RECEIVE_COMMENT:
+      newState = merge({}, newState, action.comment);
+      break;
+    case REMOVE_COMMENT:
+      delete newState[action.comment.id];
+      break;
+    case RECEIVE_ERRORS:
+      newState.errors = action.errors;
+      break;
+  }
+
+  return newState;
 };
 
 export default CommentsReducer;
