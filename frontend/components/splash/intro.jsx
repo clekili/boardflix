a5897055eee36dcd1916aa5000993323ef0ee953
can lekili <clekili@gmail.com>
2016-11-10 17:11:48 -0800

    style splash title

--- frontend/components/splash/intro.jsx
+++ frontend/components/splash/intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => (
   <div className='intro'>
     <img src="https://res.cloudinary.com/ddqzltwv6/image/upload/v1478819549/combinedReducedMore_nkc5vy.gif"/>
     <div className='splashTitle'>
-      <h1>{"Time to ride"}</h1>
+      <h1>Time to <span>ride</span></h1>
       <h2>{"Watch snow, skate, surf boarding videos. Get Ready for the season"}</h2>
     </div>
   </div>
