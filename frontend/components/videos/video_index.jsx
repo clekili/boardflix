6bd0bd3580e76601dcfbd9c06386eeb68bbcb657
can lekili <clekili@gmail.com>
2016-11-11 11:39:21 -0800

    refactor video index render function

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -31,18 +31,21 @@ class VideoIndex extends React.Component {
     }
   }
 
-  render () {
+  getSearchResults(){
     let searchResults;
     if(this.props.categories.includes("Search Results")){
       searchResults = this.buildCarousel("Search Results", false);
       let idx = this.props.categories.indexOf("Search Results");
       this.props.categories.splice(idx, 1);
     }
+    return searchResults;
+  }
 
+  render () {
     return (
       <div className="videoIndex">
         <ul>
-          {searchResults}
+          {this.getSearchResults()}
           {this.props.categories.map( category => {
             if(category !== 'errors')
               return this.buildCarousel(category);
